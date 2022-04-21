8353577a15fde28d32af69a2490cfe3a37902ee6
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-04-21 16:11:44 -0300

    refatoracao-regex-de-validacao
    As regex foram refatoradas para validacao de dados que iniciem ou terminem com espacos (char em branco).

--- src/main/java/br/com/contmatic/model/utils/constantes/ExpressoesRegulares.java
+++ src/main/java/br/com/contmatic/model/utils/constantes/ExpressoesRegulares.java
@@ -8,9 +8,9 @@ public final class ExpressoesRegulares {
     
     public static final String NUMEROS = "^\\d+$";
     public static final String NUMEROS_COM_PONTO = "([0-9.]*)";
-    public static final String LETRAS = "([A-Za-zá-úÁ-Ú., ]*)";
-    public static final String ALFANUMERICO = "([A-Za-zá-úÁ-Ú0-9., ]*)";
-    public static final String RG = "([0-9xX]*)";
+    public static final String LETRAS = "^[^\s][A-Za-zá-úÁ-Ú., ]*[^\s]$";
+    public static final String ALFANUMERICO = "^[^\s]([A-Za-zá-úÁ-Ú0-9., ]*)[^\s]$";
+    public static final String RG = "^([0-9xX]+)$";
     public static final String IP = "^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$";
     
 }
