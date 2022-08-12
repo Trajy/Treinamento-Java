a24aa168fec412e0fa1fb5d4c9ddfc1e6d45d370
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-08-11 22:01:13 -0300

    refatoracao-testes-da-classe-Endereco-para-utilizar-fixture-factory

--- src/test/java/br/com/contmatic/test/model/EnderecoTest.java
+++ src/test/java/br/com/contmatic/test/model/EnderecoTest.java
@@ -4,20 +4,18 @@ import static br.com.contmatic.test.model.fabricaobjetos.ComumErroFabricaObjetos
 import static br.com.contmatic.test.model.fabricaobjetos.ComumErroFabricaObjetos.TEXTO_COM_1_CHAR;
 import static br.com.contmatic.test.model.fabricaobjetos.ComumErroFabricaObjetos.TEXTO_COM_CHAR_NAO_ALFANUMERICO;
 import static br.com.contmatic.test.model.fabricaobjetos.ComumErroFabricaObjetos.VAZIO;
-import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.BAIRRO_01;
-import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CEP_01;
+import static br.com.contmatic.test.model.fabricaobjetos.CriacaoObjetos.construirObjetos;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CEP_02;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CEP_CHARS_REPETIDOS;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CEP_COM_LETRA;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CEP_TAMANHO_ERRADO;
-import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.CIDADE;
+import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.ENDERECO_ATRIBUTOS_OBRIGATORIOS;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.ENDERECO_NUM_ACIMA_TAMANHO_MAX;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.ENDERECO_TODOS_ATRIBUTOS;
-import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.NUMERO_ENDERECO_01;
 import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.NUMERO_ENDERECO_02;
-import static br.com.contmatic.test.model.fabricaobjetos.EnderecoFabricaObjetos.RUA_01;
-import static br.com.contmatic.test.model.fabricaobjetos.CriacaoObjetos.construirObjetos;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.INDICE_01;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.INDICE_02;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.AllOf.allOf;
 import static org.hamcrest.core.StringContains.containsString;
@@ -32,146 +30,123 @@ import org.junit.runners.MethodSorters;
 
 @FixMethodOrder(MethodSorters.JVM)
 public class EnderecoTest {
-	
-	@BeforeClass
-	public static void pre_test(){
-		construirObjetos();
-	}
-	
-	@Test
-	public void test_01_instancia_atributos_obrigatorios() {
-		assertEquals(NUMERO_ENDERECO_01, ENDERECO_ATRIBUTOS_OBRIGATORIOS.getNumero());
-		assertEquals(CEP_01, ENDERECO_ATRIBUTOS_OBRIGATORIOS.getCEP());
-	}
-	
-	@Test
-	public void test_02_instancia_todos_atributos() {
-		assertEquals(NUMERO_ENDERECO_01, ENDERECO_TODOS_ATRIBUTOS.getNumero());
-		assertEquals(CEP_01, ENDERECO_TODOS_ATRIBUTOS.getCEP());
-		assertEquals(RUA_01, ENDERECO_TODOS_ATRIBUTOS.getRua());
-		assertEquals(BAIRRO_01, ENDERECO_TODOS_ATRIBUTOS.getBairro());
-		assertEquals(CIDADE, ENDERECO_TODOS_ATRIBUTOS.getCidade());
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_03_numero_nulo() {
-		ENDERECO_TODOS_ATRIBUTOS.setNumero(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_04_numero_acima_max_chars() {
-		ENDERECO_TODOS_ATRIBUTOS.setNumero(ENDERECO_NUM_ACIMA_TAMANHO_MAX);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_05_cep_nulo() {
-		ENDERECO_TODOS_ATRIBUTOS.setCEP(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_06_cep_vazio() {
-		ENDERECO_TODOS_ATRIBUTOS.setCEP(VAZIO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_07_cep_tamanho_errado() {
-		ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_TAMANHO_ERRADO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_08_cep_com_letra() {
-		ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_COM_LETRA);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_09_cep_com_chars_repstidos() {
-		ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_CHARS_REPETIDOS);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_10_rua_nula() {
-		ENDERECO_TODOS_ATRIBUTOS.setRua(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_11_rua_vazia() {
-		ENDERECO_TODOS_ATRIBUTOS.setRua(VAZIO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_12_rua_com_char_nao_alfanumerico() {
-		ENDERECO_TODOS_ATRIBUTOS.setRua(TEXTO_COM_CHAR_NAO_ALFANUMERICO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_13_rua_abaixo_min_chars() {
-		ENDERECO_TODOS_ATRIBUTOS.setRua(TEXTO_COM_1_CHAR);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_14_rua_acima_max_chars() {
-		ENDERECO_TODOS_ATRIBUTOS.setRua(MAX_CHARS_GERAL);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_15_bairro_nulo() {
-		ENDERECO_TODOS_ATRIBUTOS.setBairro(null);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_16_bairro_vazio() {
-		ENDERECO_TODOS_ATRIBUTOS.setBairro(VAZIO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_17_bairro_com_chars_nao_alfanumerico() {
-		ENDERECO_TODOS_ATRIBUTOS.setBairro(TEXTO_COM_CHAR_NAO_ALFANUMERICO);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_18_barrio_abaixo_min_chars() {
-		ENDERECO_TODOS_ATRIBUTOS.setBairro(TEXTO_COM_1_CHAR);
-	}
-	
-	@Test(expected = IllegalStateException.class)
-	public void test_19_bairro_acima_max_chars() {
-		ENDERECO_TODOS_ATRIBUTOS.setBairro(MAX_CHARS_GERAL);
-	}
-	
-	@Test(expected = IllegalArgumentException.class)
-	public void test_20_cidade_nula() {
-		ENDERECO_TODOS_ATRIBUTOS.setCidade(null);
-	}
-	
-	@Test
-	public void test_21_metodo_hashcode() {
-		assertEquals(ENDERECO_TODOS_ATRIBUTOS.hashCode(), ENDERECO_ATRIBUTOS_OBRIGATORIOS.hashCode());
-	}
-	
-	@Test
-	public void test_22_metodo_equals() {
-		assertTrue(ENDERECO_TODOS_ATRIBUTOS.equals(ENDERECO_TODOS_ATRIBUTOS));
-		assertTrue(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(ENDERECO_TODOS_ATRIBUTOS));
-		assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(null));
-		assertFalse(ENDERECO_TODOS_ATRIBUTOS.equals(new Object()));
-		
-		ENDERECO_ATRIBUTOS_OBRIGATORIOS.setNumero(NUMERO_ENDERECO_02);
-		assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(ENDERECO_TODOS_ATRIBUTOS));
-		ENDERECO_ATRIBUTOS_OBRIGATORIOS.setCEP(CEP_02);
-		assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(ENDERECO_TODOS_ATRIBUTOS));	
-	}
-	
-	@Test
-	public void test_23_metodo_tostring() {
-		assertThat(
-			ENDERECO_TODOS_ATRIBUTOS.toString(), 
-			allOf(
-				containsString(NUMERO_ENDERECO_01.toString()),
-				containsString(CEP_01),
-				containsString(RUA_01),
-				containsString(BAIRRO_01),
-				containsString(CIDADE.toString())
-			)
-		);
-	}
+
+    @BeforeClass
+    public static void pre_test() {
+        construirObjetos();
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_03_numero_nulo() {
+        ENDERECO_TODOS_ATRIBUTOS.setNumero(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_04_numero_acima_max_chars() {
+        ENDERECO_TODOS_ATRIBUTOS.setNumero(ENDERECO_NUM_ACIMA_TAMANHO_MAX);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_05_cep_nulo() {
+        ENDERECO_TODOS_ATRIBUTOS.setCEP(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_06_cep_vazio() {
+        ENDERECO_TODOS_ATRIBUTOS.setCEP(VAZIO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_07_cep_tamanho_errado() {
+        ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_TAMANHO_ERRADO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_08_cep_com_letra() {
+        ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_COM_LETRA);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_09_cep_com_chars_repstidos() {
+        ENDERECO_TODOS_ATRIBUTOS.setCEP(CEP_CHARS_REPETIDOS);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_10_rua_nula() {
+        ENDERECO_TODOS_ATRIBUTOS.setRua(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_11_rua_vazia() {
+        ENDERECO_TODOS_ATRIBUTOS.setRua(VAZIO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_12_rua_com_char_nao_alfanumerico() {
+        ENDERECO_TODOS_ATRIBUTOS.setRua(TEXTO_COM_CHAR_NAO_ALFANUMERICO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_13_rua_abaixo_min_chars() {
+        ENDERECO_TODOS_ATRIBUTOS.setRua(TEXTO_COM_1_CHAR);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_14_rua_acima_max_chars() {
+        ENDERECO_TODOS_ATRIBUTOS.setRua(MAX_CHARS_GERAL);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_15_bairro_nulo() {
+        ENDERECO_TODOS_ATRIBUTOS.setBairro(null);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_16_bairro_vazio() {
+        ENDERECO_TODOS_ATRIBUTOS.setBairro(VAZIO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_17_bairro_com_chars_nao_alfanumerico() {
+        ENDERECO_TODOS_ATRIBUTOS.setBairro(TEXTO_COM_CHAR_NAO_ALFANUMERICO);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_18_barrio_abaixo_min_chars() {
+        ENDERECO_TODOS_ATRIBUTOS.setBairro(TEXTO_COM_1_CHAR);
+    }
+
+    @Test(expected = IllegalStateException.class)
+    public void test_19_bairro_acima_max_chars() {
+        ENDERECO_TODOS_ATRIBUTOS.setBairro(MAX_CHARS_GERAL);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_20_cidade_nula() {
+        ENDERECO_TODOS_ATRIBUTOS.setCidade(null);
+    }
+
+    @Test
+    public void test_21_metodo_hashcode() {
+        assertEquals(ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01).hashCode(), ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_02).hashCode());
+    }
+
+    @Test
+    public void test_22_metodo_equals() {
+        assertTrue(ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01).equals(ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01)));
+        assertTrue(ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01).equals(ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_02)));
+        assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(null));
+        assertFalse(ENDERECO_TODOS_ATRIBUTOS.equals(new Object()));
+
+        ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01).setNumero(NUMERO_ENDERECO_02);
+        assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(ENDERECO_TODOS_ATRIBUTOS));
+        ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS.get(INDICE_01).setCEP(CEP_02);
+        assertFalse(ENDERECO_ATRIBUTOS_OBRIGATORIOS.equals(ENDERECO_TODOS_ATRIBUTOS));
+    }
+
+    @Test
+    public void test_23_metodo_tostring() {
+        assertThat(ENDERECO_TODOS_ATRIBUTOS.toString(), allOf(containsString(ENDERECO_TODOS_ATRIBUTOS.getNumero().toString()), containsString(ENDERECO_TODOS_ATRIBUTOS.getCep()),
+            containsString(ENDERECO_TODOS_ATRIBUTOS.getRua()), containsString(ENDERECO_TODOS_ATRIBUTOS.getBairro()), containsString(ENDERECO_TODOS_ATRIBUTOS.getCidade().toString())));
+    }
 }
--- src/test/java/br/com/contmatic/test/model/fabricaobjetos/EnderecoFabricaObjetos.java
+++ src/test/java/br/com/contmatic/test/model/fabricaobjetos/EnderecoFabricaObjetos.java
@@ -3,52 +3,68 @@ package br.com.contmatic.test.model.fabricaobjetos;
 import java.util.ArrayList;
 import java.util.List;
 
+
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.fabricarObjeto;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.gerarNumerosAleatorios;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.obterObjeto;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.obterVariosObjetos;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.gerarAPartirRegex;
+import static br.com.contmatic.test.model.fabricaobjetos.FabricaObjetosUtils.gerarLetrasAleatorias;
+import static br.com.contmatic.test.model.fabricaobjetos.ObjetoFixtureTemplate.OBRIGATORIOS_ARGS;
+import static br.com.contmatic.test.model.fabricaobjetos.ObjetoFixtureTemplate.TODOS_ARGS;
 import br.com.contmatic.model.endereco.Cidade;
 import br.com.contmatic.model.endereco.Endereco;
+import br.com.six2six.fixturefactory.Rule;
+
+import static java.lang.Integer.parseInt;
+
 import static br.com.contmatic.test.model.fabricaobjetos.UnidadeFederativaFabricaObjetos.UF_01;
 
 public final class EnderecoFabricaObjetos {
-    
+
     private EnderecoFabricaObjetos() {
-        
+
     }
+
+    public static final Integer NUMERO_ENDERECO_01;
+    public static final Integer NUMERO_ENDERECO_02;
+    public static final Integer NUMERO_ENDERECO_03;
+    public static final Integer NUMERO_ENDERECO_04;
+    public static final Integer NUMERO_ENDERECO_05;
+    public static final String CEP_01;
+    public static final String CEP_02;
+    public static final String CEP_03;
+    public static final String CEP_04;
+    public static final String CEP_05;
+    public static final String RUA_01;
+    public static final String RUA_02;
+    public static final String BAIRRO_01;
+    public static final String BAIRRO_02;
+    public static final String NOME_CIDADE;
+    public static final String NOME_CIDADE_02;
+    public static final String PAIS;
+    public static final String PAIS_02;
+    public static final Cidade CIDADE;
+    public static final Cidade OUTRA_CIDADE;
+    public static final Endereco ENDERECO_01;
+    public static final Endereco ENDERECO_02;
+    public static final Endereco ENDERECO_03;
+    public static final Endereco ENDERECO_04;
+    public static final Endereco ENDERECO_05;
+    public static final Endereco ENDERECO_ATRIBUTOS_OBRIGATORIOS;
     
-    public static Integer NUMERO_ENDERECO_01;
-    public static Integer NUMERO_ENDERECO_02;
-    public static Integer NUMERO_ENDERECO_03;
-    public static Integer NUMERO_ENDERECO_04;
-    public static Integer NUMERO_ENDERECO_05;
-    public static String CEP_01;
-    public static String CEP_02;
-    public static String CEP_03;
-    public static String CEP_04;
-    public static String CEP_05;
-    public static String RUA_01;
-    public static String RUA_02;
-    public static String BAIRRO_01;
-    public static String BAIRRO_02;
-    public static String NOME_CIDADE;
-    public static String NOME_CIDADE_02;
-    public static String PAIS;
-    public static String PAIS_02;
-    public static Cidade CIDADE;
-    public static Cidade OUTRA_CIDADE;
-    public static Endereco ENDERECO_01;
-    public static Endereco ENDERECO_02;
-    public static Endereco ENDERECO_03;
-    public static Endereco ENDERECO_04;
-    public static Endereco ENDERECO_05;
-    public static Endereco ENDERECO_ATRIBUTOS_OBRIGATORIOS;
-    public static Endereco ENDERECO_TODOS_ATRIBUTOS;
-    public static List<Endereco> ENDERECOS;
-    public static List<Endereco> LISTA_ENDERECOS_ACIMA_LIMITE;
-    
-    public static String CEP_CHARS_REPETIDOS;
-    public static String CEP_COM_LETRA;
-    public static String CEP_TAMANHO_ERRADO;
-    public static Integer ENDERECO_NUM_ACIMA_TAMANHO_MAX;
-    
-    static void construirEndereco() {
+    public static final List<Endereco> ENDERECOS;
+    public static final List<Endereco> LISTA_ENDERECOS_ACIMA_LIMITE;
+
+    public static final String CEP_CHARS_REPETIDOS;
+    public static final String CEP_COM_LETRA;
+    public static final String CEP_TAMANHO_ERRADO;
+    public static final Integer ENDERECO_NUM_ACIMA_TAMANHO_MAX;
+     
+    public static final Endereco ENDERECO_TODOS_ATRIBUTOS;
+    public static final List<Endereco> ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS;
+
+    static {
         NUMERO_ENDERECO_01 = 1000;
         NUMERO_ENDERECO_02 = 22;
         NUMERO_ENDERECO_03 = 4000;
@@ -68,14 +84,21 @@ public final class EnderecoFabricaObjetos {
         PAIS = "Brasil";
         PAIS_02 = "Chile";
         CIDADE = new Cidade(NOME_CIDADE, UF_01, PAIS);
+        
+        Class<Endereco> classe = Endereco.class;
+        fabricarEnderecos(classe);
+        final int QTD_OBJETOS_IGUAIS = 2;
+        ENDERECO_TODOS_ATRIBUTOS = obterObjeto(classe, TODOS_ARGS);
+        ENDERECOS_IGUAIS_ATRIBUTOS_OBRIGATORIOS = obterVariosObjetos(classe, OBRIGATORIOS_ARGS, QTD_OBJETOS_IGUAIS);
+        
         OUTRA_CIDADE = new Cidade(NOME_CIDADE, UF_01, PAIS);
-        ENDERECO_01 = new Endereco(NUMERO_ENDERECO_01, CEP_01, RUA_01,BAIRRO_01 , CIDADE);
+        ENDERECO_01 = new Endereco(NUMERO_ENDERECO_01, CEP_01, RUA_01, BAIRRO_01, CIDADE);
         ENDERECO_02 = new Endereco(NUMERO_ENDERECO_02, CEP_02, RUA_02, BAIRRO_02, CIDADE);
-        ENDERECO_03 = (new Endereco(NUMERO_ENDERECO_03,CEP_03));
+        ENDERECO_03 = (new Endereco(NUMERO_ENDERECO_03, CEP_03));
         ENDERECO_04 = (new Endereco(NUMERO_ENDERECO_04, CEP_04));
         ENDERECO_05 = new Endereco(NUMERO_ENDERECO_05, CEP_05);
         ENDERECO_ATRIBUTOS_OBRIGATORIOS = new Endereco(NUMERO_ENDERECO_01, CEP_01);
-        ENDERECO_TODOS_ATRIBUTOS = new Endereco(NUMERO_ENDERECO_01, CEP_01, RUA_01, BAIRRO_01, CIDADE);
+        
         ENDERECOS = new ArrayList<>();
         ENDERECOS.add(ENDERECO_01);
         ENDERECOS.add(ENDERECO_02);
@@ -85,11 +108,51 @@ public final class EnderecoFabricaObjetos {
         LISTA_ENDERECOS_ACIMA_LIMITE = new ArrayList<>();
         LISTA_ENDERECOS_ACIMA_LIMITE.addAll(ENDERECOS);
         LISTA_ENDERECOS_ACIMA_LIMITE.add(ENDERECO_01);
-        
+
         CEP_CHARS_REPETIDOS = "11111111";
         CEP_COM_LETRA = "123a5678";
         CEP_TAMANHO_ERRADO = "123901203";
         ENDERECO_NUM_ACIMA_TAMANHO_MAX = 123456789;
+    }
+
+    static void construirEndereco() {
         
+    }
+
+    private static void fabricarEnderecos(Class classe) {
+        fabricarObjeto(classe, TODOS_ARGS, new Rule() {{
+            add("numero", gerarNumero());
+            add("cep", gerarCep());
+            add("rua", gerarRua());
+            add("bairro", gerarLetrasComTamanhoPadraoEndereco());
+            add("cidade", CIDADE);
+        }});
+        
+        fabricarObjeto(classe, OBRIGATORIOS_ARGS, new Rule() {{
+            add("numero", gerarNumero());
+            add("cep", gerarCep());
+        }});
+    }
+    
+    private static Integer gerarNumero() {
+        final String EXPRESSAO_REGULAR_NUMERO = "[1-9]{1,8}";
+        return parseInt(gerarAPartirRegex(EXPRESSAO_REGULAR_NUMERO));
+    }
+    
+    private static String gerarCep() {
+        final int CEP_TAMANHO = 8;
+        return gerarNumerosAleatorios(CEP_TAMANHO);
+    }
+    
+    private static String gerarRua() {
+        final int RUA_TAMANHO_MIN = 2;
+        final int RUA_TAMANHO_MAX = 60;
+        return gerarLetrasAleatorias(RUA_TAMANHO_MIN, RUA_TAMANHO_MAX);
+    }
+    
+    private static String gerarLetrasComTamanhoPadraoEndereco() {
+        final int TAMANHO_MIN_PADRAO_ENDERECO = 2;
+        final int TAMANHO_MAX_PADRAO_ENDEROCO = 25;
+        return gerarLetrasAleatorias(TAMANHO_MIN_PADRAO_ENDERECO, TAMANHO_MAX_PADRAO_ENDEROCO);
     }
 }
