be78ce830b843b851f9f360ed716e8985125d310
Henrique Trajano de Araujo <henrique.trajano.a@gmail.com>
2022-09-11 02:09:38 -0300

    refatoracao-ValidacaoUtils

--- src/test/java/br/com/contmatic/test/util/ValidacaoTesteUtils.java
+++ src/test/java/br/com/contmatic/test/util/ValidacaoTesteUtils.java
@@ -1,18 +1,20 @@
 package br.com.contmatic.test.util;
 
+import static java.util.Objects.nonNull;
+import static java.util.stream.Stream.of;
+import static org.apache.commons.collections4.IterableUtils.first;
+import static org.apache.commons.collections4.IterableUtils.find;
+import static org.apache.commons.lang3.ObjectUtils.allNotNull;
+import static org.apache.commons.lang3.ObjectUtils.notEqual;
+import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ONE;
+import static org.apache.commons.lang3.math.NumberUtils.INTEGER_ZERO;
+
 import java.lang.reflect.Field;
 import java.util.List;
-import static java.util.stream.Stream.of;
 
 import javax.validation.ConstraintViolation;
 import javax.validation.Validation;
 import javax.validation.Validator;
-import javax.validation.metadata.ConstraintDescriptor;
-
-import static org.apache.commons.collections4.IterableUtils.first;
-import static org.apache.commons.lang3.ObjectUtils.notEqual;
-import static org.apache.commons.lang3.ObjectUtils.allNotNull;
-
 
 import com.google.gson.Gson;
 
@@ -24,12 +26,7 @@ public class ValidacaoTesteUtils {
         final Class<T> classe = (Class<T>) objetoTeste.getClass();
         final T copiaObjetoTeste = copiarObjeto(objetoTeste, classe);
         funcao.run();
-        verificaSeAtributoPossuiViolacoes(objetoTeste, annotationClass, qualAtributoAlterado(objetoTeste, copiaObjetoTeste));
-    }
-    
-    public static <T> void validarAnnotation(T objetoTeste, Class annotationClass, String nomeAtributo) {
-        verificaSeObjetoPossuiAtributo(objetoTeste, nomeAtributo);
-        verificaSeAtributoPossuiViolacoes(objetoTeste, annotationClass, nomeAtributo);       
+        encontrarAtributoAlterado(objetoTeste, copiaObjetoTeste, annotationClass);
     }
     
     private static <T> T copiarObjeto(T objeto, Class<T> classe) {
@@ -39,7 +36,7 @@ public class ValidacaoTesteUtils {
         return mapper.fromJson(mapper.toJson(objeto), classe);
     }
     
-    private static <T> String qualAtributoAlterado(T objetoTeste, T copiaObjetoTeste) {
+    private static <T> void encontrarAtributoAlterado(T objetoTeste, T copiaObjetoTeste, Class annotationClass) {
         System.out.println(of(objetoTeste.getClass().getDeclaredFields()).toList() + "original");
         System.out.println(of(copiaObjetoTeste.getClass().getDeclaredFields()).toList() + "copiado papai");
         List<Field> atributosComValorDiferente = of(objetoTeste.getClass().getDeclaredFields())
@@ -58,37 +55,55 @@ public class ValidacaoTesteUtils {
                     }
                 }).toList();
         System.out.println(atributosComValorDiferente + "olha nois");
-        if(atributosComValorDiferente.size() > 1) {
-            throw new AssertionError("Mais de um atributo foi alterado, neste caso altere apenas um atributo ou informe qual atributo deseja verificar como argumento");
-        }
-        return first(atributosComValorDiferente).getName();   
+        validarAlteracaoUnicoAtributo(atributosComValorDiferente);
+        verificarViolacao(objetoTeste, annotationClass, first(atributosComValorDiferente).getName());
+        System.out.println(first(atributosComValorDiferente).getName() + "aki ooo");
+        retornarValorInicial(objetoTeste, copiaObjetoTeste, first(atributosComValorDiferente));
     }
     
-    private static void verificaSeObjetoPossuiAtributo(Object objetoTeste, String nomeAtributo) {
-        if(of(objetoTeste.getClass().getDeclaredFields()).anyMatch((atributo) -> atributo.getName().equals(nomeAtributo))) {
+    private static void validarAlteracaoUnicoAtributo(List<Field> atributosComValorDiferente) {
+        if(atributosComValorDiferente.size() > INTEGER_ONE) {
+            throw new AssertionError("Mais de um atributo foi alterado altere apenas um atributo");
+        }
+    }
+    
+    private static <T> void verificarViolacao(T objetoTeste, Class annotationClass, String nomeAtributo) {
+        List<ConstraintViolation<T>> todasViolacoes = validador.validate(objetoTeste).stream().toList();
+        verificarSeExisteViolacao(todasViolacoes);
+        ConstraintViolation<T> violacaoAtributoAlterado = find(  
+            todasViolacoes.stream()
+                .filter((violacao) -> violacao.getPropertyPath().toString().equals(nomeAtributo))
+                .toList(),
+                (violacao) -> violacao.getConstraintDescriptor().getAnnotation().annotationType().equals(annotationClass)    
+        );
+        System.out.println(violacaoAtributoAlterado);
+        System.out.println(annotationClass);
+        if(nonNull(violacaoAtributoAlterado)) {
             return;
         }
         StringBuilder mensagem = new StringBuilder()
-                .append("A classe ").append(objetoTeste.getClass())
-                .append(" nao possui o atributo '").append(nomeAtributo).append("'\n\n");
+                .append("A seguinte validacao era esperada: ").append(annotationClass).append("\n")
+                .append("entretando as seguintes validacoes foram obtidas\n");
+                for(ConstraintViolation<T> violacao : todasViolacoes) {
+                    mensagem.append(violacao.getConstraintDescriptor().getAnnotation()).append("\n");
+                }
+                mensagem.append("atributo: ").append(first(todasViolacoes).getPropertyPath()).append("\n")
+                .append("valor: ").append(first(todasViolacoes).getInvalidValue()).append("\n");
         throw new AssertionError(mensagem.toString());
     }
     
-    private static void verificaSeAtributoPossuiViolacoes(Object objetoTeste, Class annotationClass, String nomeAtributo) {
-        for(ConstraintViolation<?> validacao :  validador.validate(objetoTeste).stream().toList()) {
-            ConstraintDescriptor<?> erroMetadata = validacao.getConstraintDescriptor();
-            if(validacao.getPropertyPath().toString().equals(nomeAtributo)) {
-                if(erroMetadata.getAnnotation().annotationType().equals(annotationClass)) {
-                    return;
-                }
-                StringBuilder mensagem = new StringBuilder()
-                        .append("A seguinte validacao falhou\n")
-                        .append(erroMetadata.getAnnotation()).append("\n")
-                        .append("atributo: ").append(validacao.getPropertyPath()).append("\n")
-                        .append("valor: ").append(validacao.getInvalidValue()).append("\n");
-                throw new AssertionError(mensagem.toString());
-            }
-        }  
+    private static <T> void verificarSeExisteViolacao(List<ConstraintViolation<T>> violacoes) {
+        if(violacoes.size() == INTEGER_ZERO) {
+            throw new AssertionError("Nenhuma validacao obtida");
+        }
+    }
+    
+    private static <T> void retornarValorInicial(T objetoTeste, T copiaObjetoTeste, Field atributoAlterado) {
+        try {
+            atributoAlterado.set(objetoTeste, atributoAlterado.get(copiaObjetoTeste));
+        } catch (IllegalArgumentException | IllegalAccessException e) {
+            throw new AssertionError("Erro ao retornar valor inicial do atributo");
+        }
     }
     
 }
